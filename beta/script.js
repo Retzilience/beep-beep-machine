83c8499cae9ac510bc331573be8651414c9e8b22
Retzilience <renato.slf+git@gmail.com>
2023-03-12 08:35:55 -0300

    !

--- beta/script.js
+++ beta/script.js
@@ -24,12 +24,59 @@ beat.start();
 beat.stop(audioContext.currentTime + duration);
 var isPlaying = false;
 var bpmInput = document.getElementById("bpm");
-bpmInput.addEventListener("input", function () {
-  var bpm = parseInt(this.value);
-  if (bpm >= 1 && bpm <= 300) {
-    updateBpmValue(bpm);
+// New code starts here
+// Declare a new variable to store the ID of the updateInterval timeout
+var updateInterval;
+
+// Declare a new function to update the metronome interval
+function updateMetronomeInterval() {
+  // Check if the metronome is currently playing
+  if (isPlaying) {
+    // If it is playing, clear the existing timer
+    clearInterval(timer);
+
+    // Calculate the new interval based on the current BPM value
+    var bpm = parseInt(bpmInput.value);
+    var interval = 60000 / bpm;
+
+    // Set a new timer with the updated interval
+    timer = setInterval(function () {
+      beat = audioContext.createOscillator();
+      beat.frequency.value = 1000;
+      beat.connect(gainNode);
+      beat.start(audioContext.currentTime);
+      beat.stop(audioContext.currentTime + duration);
+      circle.style.opacity = "1.0";
+      setTimeout(function () {
+        circle.style.opacity = "0.0";
+      }, interval / 2);
+    }, interval);
   }
-});
+}
+
+// New code ends here
+
+bpmInput.addEventListener("input", function () {
+
+  // Updated event listener starts here
+  bpmInput.addEventListener("input", function () {
+    // Get the current BPM value from the input field
+    var bpm = parseInt(this.value);
+  
+    // Check if the BPM value is within a valid range
+    if (bpm >= 1 && bpm <= 300) {
+      // Update the displayed BPM value
+      updateBpmValue(bpm);
+  
+      // Clear any existing timeout set by updateInterval
+      clearTimeout(updateInterval);
+
+      // Set a new timeout to call updateMetronomeInterval after a delay
+      // This prevents rapid updates while the user is still typing
+      updateInterval = setTimeout(updateMetronomeInterval, 500);
+    }
+  });
+// Updated event listener ends here
 var startButton = document.getElementById("start");
 var stopButton = document.getElementById("stop");
 var circle = document.querySelector(".circle");
@@ -62,19 +109,8 @@ var tapButton = document.getElementById("tap");
 var tapTimes = [];
 var tapTimer;
 tapButton.addEventListener("click", function () {
-  // Push the current time to the tapTimes array
   tapTimes.push(Date.now());
-  
-  // If there are more than 4 times in the array, remove the first one
-  if (tapTimes.length > 4) {
-    tapTimes.shift();
-  }
-  
-  // Clear any existing timeout set by tapTimer
   clearTimeout(tapTimer);
-  
-  // If there are more than two times in the array,
-  // calculate an average interval between taps and set BPM value accordingly
   if (tapTimes.length > 2) {
     var total = 0;
     for (var i = 1; i < tapTimes.length; i++) {

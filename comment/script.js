98d1aefe5791f8da15158f8d8b55ad0bf65171ce
Retzilience <renato.slf+git@gmail.com>
2023-03-12 19:49:39 -0300

    !

--- comment/script.js
+++ comment/script.js
@@ -40,18 +40,46 @@ var bpmInput = document.getElementById("bpm");
 var startButton = document.getElementById("start");
 var stopButton = document.getElementById("stop");
 var circle = document.querySelector(".circle");
-
 // Sets up event listeners for various user interface elements such as buttons for starting and stopping the metronome, adjusting the beats per minute (BPM), and tapping to set the BPM.
 // When the start button is clicked, if the metronome is not already playing, an interval timer is set up based on the current BPM value. At each interval, an oscillator node is created and started to produce a sound at 1000 Hz for 0.025 seconds. A visual indicator (circle) also briefly changes opacity.
 // When the stop button is clicked, if the metronome is playing, it stops by clearing the interval timer.
-
 // Update BPM value when user changes input field value
+// If the metronome is playing and the new BPM value is different from the previous one, update the interval timer to reflect the new BPM value
+// A timeout is added to prevent immediate changes in BPM from interfering with the current interval timer
 bpmInput.addEventListener("input", function () {
+  var lastBpm = 0;
+  bpmInput.addEventListener("change", function () {
+    if (isPlaying && parseInt(this.value) !== lastBpm) {
+      setTimeout(function () {
+        var bpm = parseInt(bpmInput.value);
+        var interval = 60000 / bpm;
+        clearInterval(timer);
+        timer = setInterval(function () {
+          beat = audioContext.createOscillator();
+          beat.frequency.value = 1000;
+          beat.connect(gainNode);
+          beat.start(audioContext.currentTime);
+          beat.stop(audioContext.currentTime + duration);
+          circle.style.opacity = "1.0";
+          setTimeout(function () {
+            circle.style.opacity = "0.0";
+          }, interval / 2);
+        }, interval);
+      }, 500);
+      lastBpm = parseInt(this.value);
+    }
+  });
   var bpm = parseInt(this.value);
   if (bpm >= 1 && bpm <= 300) {
     updateBpmValue(bpm);
   }
 });
+// Get the start, stop buttons and circle element from the DOM
+
+var startButton = document.getElementById("start");
+var stopButton = document.getElementById("stop");
+var circle = document.querySelector(".circle");
+
 
 // Start playing the metronome when start button is clicked.
 startButton.addEventListener("click", function () {
@@ -83,7 +111,32 @@ stopButton.addEventListener("click", function () {
     circle.style.opacity = "0.0";
   }
 });
-
+/*
+This function updates the metronome's interval based on the user's input
+It clears the current timer and sets a new one with the updated interval
+It also creates an oscillator and connects it to the gain node, which is connected to the destination (the speakers)
+The oscillator's frequency is set to 1000 Hz, which will produce a click sound
+After starting the oscillator, it will stop after a specified duration
+The circle element's opacity is set to 1 to indicate the start of a beat, and then set to 0 after half the interval to indicate the end of a beat
+*/
+function updateMetronomeInterval() {
+  if (isPlaying) {
+    clearInterval(timer);
+    var bpm = parseInt(bpmInput.value);
+    var interval = 60000 / bpm;
+    timer = setInterval(function () {
+      beat = audioContext.createOscillator();
+      beat.frequency.value = 1000;
+      beat.connect(gainNode);
+      beat.start(audioContext.currentTime);
+      beat.stop(audioContext.currentTime + duration);
+      circle.style.opacity = "1.0";
+      setTimeout(function () {
+        circle.style.opacity = "0.0";
+      }, interval / 2);
+    }, interval);
+  }
+}
 // The tap button allows users to tap in time with their desired BPM. Each time it’s clicked, it records the current time in an array (tapTimes). If more than two taps have been recorded within two seconds (determined by a timeout), it calculates an average interval between taps and sets the BPM value accordingly.
 // The tapButton variable is assigned to the element with the ID “tap”. An empty array tapTimes and a variable tapTimer are also declared.
 // An event listener is added to the tap button for click events. When clicked, the current time in milliseconds since January 1, 1970 (obtained using Date.now()) is pushed to the tapTimes array. Any existing timeout set by tapTimer is cleared using clearTimeout().
@@ -96,7 +149,8 @@ var tapTimer;
 tapButton.addEventListener("click", function () {
   tapTimes.push(Date.now());
   clearTimeout(tapTimer);
-  if (tapTimes.length > 2) {
+  if (tapTimes.length > 3) {
+    tapTimes.shift();
     var total = 0;
     for (var i = 1; i < tapTimes.length; i++) {
       total += tapTimes[i] - tapTimes[i - 1];
@@ -104,6 +158,7 @@ tapButton.addEventListener("click", function () {
     var average = total / (tapTimes.length - 1);
     var bpm = Math.round(60000 / average);
     bpmInput.value = bpm;
+    updateMetronomeInterval();
   }
   tapTimer = setTimeout(function () {
     tapTimes = [];
@@ -117,12 +172,14 @@ bpmDecreaseButton.addEventListener("click", function () {
   var bpm = parseInt(bpmInput.value);
   if (bpm > 1) {
     bpmInput.value = bpm - 1;
+    updateMetronomeInterval();
   }
 });
 bpmIncreaseButton.addEventListener("click", function () {
   var bpm = parseInt(bpmInput.value);
   if (bpm < 300) {
     bpmInput.value = bpm + 1;
+    updateMetronomeInterval();
   }
 });
 
